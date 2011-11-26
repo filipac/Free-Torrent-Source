0ec52c89ea785ad9c08736602ae83bad94495423
filipac <hello@filipac.net>
2011-11-27 01:17:37 +0200

    extra space

--- include/cron_failed_logins.php
+++ include/cron_failed_logins.php
@@ -9,4 +9,4 @@
     $secs = 1 * 86400 ; // Delete failed login attempts per one day.
     $dt = sqlesc( get_date_time(gmtime() - $secs) ) ; // calculate date.
     mysql_query( "DELETE FROM loginattempts WHERE banned='no' AND added < $dt" ) ; // do job.
-    ?>
+?>
